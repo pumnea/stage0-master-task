229f370f023188a593677a308da5cb7dc65add96
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-05-06 16:44:50 +0300

    add: langSyntax Task 1

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        // convert int to string
+        String str = Integer.toString(number);
+        // reverse string
+        String reversed = new StringBuilder(str).reverse().toString();
+        // convert string to int
+        int result = Integer.parseInt(reversed);
     }
 
 }
