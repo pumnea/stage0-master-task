05455ce86015d1388d308e411bfe3c3753276607
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-05-06 16:49:57 +0300

    add: langSyntax Task 2

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,10 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int result = String.valueOf(number)
+                .chars()
+                .map(Character::getNumericValue)
+                .sum();
     }
 
 }
