3a6e25d8c09ec2dc24d28da568bbc7bcad5ae8e8
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-06-11 17:23:20 +0300

    add: solution for half pyramid task.

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,13 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
