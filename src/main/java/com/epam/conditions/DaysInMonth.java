51269c2b6b67aa0b1382e6ea7dd5654cfc26d1c5
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-06-11 17:00:20 +0300

    add: solution for days in a month task.

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,17 @@
 package com.epam.conditions;
 
+import java.time.YearMonth;
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+        }
 
+        YearMonth yearMonth = YearMonth.of(year, month);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        System.out.println(daysInMonth);
     }
 
 }
