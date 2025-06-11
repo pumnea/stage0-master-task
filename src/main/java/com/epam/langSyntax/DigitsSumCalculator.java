78f77ae09904550b12656cdec5b1fcae79c5983f
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-06-11 18:38:17 +0300

    upd: rm libs and string methods.

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,11 +3,18 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-        int result = String.valueOf(number)
-                .chars()
-                .map(Character::getNumericValue)
-                .sum();
-        System.out.println(result);
+        int sum = 0;
+        if (number < 0) {
+            // Handle negative numbers
+            number = -number;
+        }
+        while (number > 0) {
+            // Extract the last digit
+            sum += number % 10;
+            // Remove the last digit
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 
 }
