687a6dbc59787f06d42fa7d80bd60b3f004b44dc
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-06-11 18:17:33 +0300

    upd: sout result

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,13 +2,13 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
-    public int revert(int number) {
+    public void revert(int number) {
         // convert int to string
         String str = Integer.toString(number);
         // reverse string
         String reversed = new StringBuilder(str).reverse().toString();
         // convert string to int
-        return Integer.parseInt(reversed);
+        System.out.println(Integer.parseInt(reversed));
     }
 
 }
