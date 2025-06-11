bf02fd5211f865f9380b0bf83b01cbdb6e9e0713
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-06-11 18:31:58 +0300

    upd: reformat output results.

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -5,10 +5,10 @@ import java.time.YearMonth;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-        if (month < 1 || month > 12) {
+        if (month < 1 || month > 12 || year <= 0) {
             System.out.println("invalid date");
+            return;
         }
-
         YearMonth yearMonth = YearMonth.of(year, month);
         int daysInMonth = yearMonth.lengthOfMonth();
         System.out.println(daysInMonth);
